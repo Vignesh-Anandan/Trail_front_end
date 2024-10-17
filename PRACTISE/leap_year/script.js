03f791d5410e665a661ef7a789f9d03ccf0ff7b2
Vignesh-Anandan <vignesh.a0695@gmail.com>
2024-10-17 13:47:54 +0530

    Revert "October_backup-01"

--- PRACTISE/leap_year/script.js
+++
@@ -1,6 +0,0 @@
-//Write a JavaScript program 
-//to determine whether a given year is a leap year in the Gregorian calendar.  function myFunction() {
-      function fromuser() {
-        var year = document.getElementById("fourdigit").value;
-        document.write(year);
-    }
